81c1192bd4f3a4b0dbdfcd09737644e9ecc77e42
kikones34 <kikones34@gmail.com>
2026-01-31 20:08:11 +0100

    Player cleanup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,19 +10,6 @@ public class PlayerController : MonoBehaviour
     [Header("Moving")] //###############################################################################################
     public float baseSpeed = 15;
 
-    public float accelerationTime;
-    public AnimationCurve accelerationCurve;
-    public float deaccelerationTime;
-    public AnimationCurve deaccelerationCurve;
-
-    private float plusRotValue;
-
-    private float lastInputH;
-    private float lastInputV;
-    private bool rotatedThisUpdate = false;
-
-    private float _timeSinceLastStop;
-    private float _timeSinceLastMove;
     private bool _isMoving;
     
     [Header("References")] //#####################################################################################################
@@ -31,14 +18,9 @@ public class PlayerController : MonoBehaviour
     public Animator animator;
     
     private Transform _trans;
-    private Rigidbody2D _rigidbody;
-    private CircleCollider2D _collider;
     private UnityEngine.AI.NavMeshAgent _navMeshAgent;
-    
-    private float _floatingTolerance = 0.001f;
-    private Vector2 _last4WayDir;
 
-    
+
     private void Awake()
     {
         if (Instance != null)
@@ -53,16 +35,12 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         _trans = transform;
-        _collider = GetComponent<CircleCollider2D>();
-        _rigidbody = GetComponent<Rigidbody2D>();
         _navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
 
         _navMeshAgent.updateRotation = false;
         _navMeshAgent.updateUpAxis = false;
 
         _isMoving = false;
-        _timeSinceLastMove = 0;
-        _timeSinceLastStop = 0;
     }
     
     void Update()
@@ -78,17 +56,12 @@ public class PlayerController : MonoBehaviour
         if (UserInput.Instance.GoToPointPressedThisFrame)
         {
             _navMeshAgent.SetDestination(UserInput.Instance.MoveTarget);
-            _timeSinceLastStop = 0;
-            _timeSinceLastMove = 0;
         }
 
         _isMoving = _navMeshAgent.velocity.magnitude > 0.1f;
         animator.SetBool("isMoving", _isMoving);
 
         if (_isMoving) UpdateMoveRotation();
-        
-        // This is overwriting the full velocity of the Rigidbody system. This is not the best, but gives us the most control.
-        // _rigidbody.linearVelocity = newFullForce;
     }
     
     private void UpdateMoveRotation()
